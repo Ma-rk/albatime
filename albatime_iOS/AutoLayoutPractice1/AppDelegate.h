d849a2773589e948dfdf6266737488551e7b5e37
VincentGigandet <uvgone@gmail.com>
2015-06-21 21:44:12 +0900

    dd

---
+++ albatime_iOS/AutoLayoutPractice1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutPractice1
+//
+//  Created by VincentGigandet on 2015. 6. 11..
+//  Copyright (c) 2015년 VincentGigandet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
