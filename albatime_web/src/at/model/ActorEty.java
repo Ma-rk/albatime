ec25e31e12b02c31ae5ac324cb1ed4f3c73384ce
Mark K <k.mark.kr@gmail.com>
2015-07-22 20:35:09 +0900

    applied lombok library. close #48

--- albatime_web/src/at/model/ActorEty.java
+++ albatime_web/src/at/model/ActorEty.java
@@ -12,7 +12,13 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import at.supp.CC;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ActorEty {
 
 	private long seq;
@@ -60,33 +66,6 @@ public class ActorEty {
 	private Date created;
 	private Date edited;
 
-	public ActorEty() {}
-
-	public ActorEty(long seq, long userId, String name, String memo, Date periodFrom, Date periodTo, int workTimeUnit,
-			int alarmBefore, String unpaidbreakFlag, float taxRate, float basicWage, String bgColor, String phone1,
-			String phone2, String addr1, String addr2, String addr3, String stus, Date created, Date edited) {
-		this.seq = seq;
-		this.userId = userId;
-		this.name = name;
-		this.memo = memo;
-		this.periodFrom = periodFrom;
-		this.periodTo = periodTo;
-		this.workTimeUnit = workTimeUnit;
-		this.alarmBefore = alarmBefore;
-		this.unpaidbreakFlag = unpaidbreakFlag;
-		this.taxRate = taxRate;
-		this.basicWage = basicWage;
-		this.bgColor = bgColor;
-		this.phone1 = phone1;
-		this.phone2 = phone2;
-		this.addr1 = addr1;
-		this.addr2 = addr2;
-		this.addr3 = addr3;
-		this.stus = stus;
-		this.created = created;
-		this.edited = edited;
-	}
-
 	public void setAsNormalStus() {
 		this.stus = CC.ACTOR_STUS_NORMAL;
 	}
@@ -94,178 +73,4 @@ public class ActorEty {
 	public void setAsDeactivatedStus() {
 		this.stus = CC.ACTOR_STUS_DELETED;
 	}
-
-	/*
-	 * getters and setters
-	 */
-	public long getSeq() {
-		return seq;
-	}
-
-	public void setSeq(long seq) {
-		this.seq = seq;
-	}
-
-	public long getUserId() {
-		return userId;
-	}
-
-	public void setUserId(long userId) {
-		this.userId = userId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getMemo() {
-		return memo;
-	}
-
-	public void setMemo(String memo) {
-		this.memo = memo;
-	}
-
-	public Date getPeriodFrom() {
-		return periodFrom;
-	}
-
-	public void setPeriodFrom(Date periodFrom) {
-		this.periodFrom = periodFrom;
-	}
-
-	public Date getPeriodTo() {
-		return periodTo;
-	}
-
-	public void setPeriodTo(Date periodTo) {
-		this.periodTo = periodTo;
-	}
-
-	public int getWorkTimeUnit() {
-		return workTimeUnit;
-	}
-
-	public void setWorkTimeUnit(int workTimeUnit) {
-		this.workTimeUnit = workTimeUnit;
-	}
-
-	public int getAlarmBefore() {
-		return alarmBefore;
-	}
-
-	public void setAlarmBefore(int alarmBefore) {
-		this.alarmBefore = alarmBefore;
-	}
-
-	public String getUnpaidbreakFlag() {
-		return unpaidbreakFlag;
-	}
-
-	public void setUnpaidbreakFlag(String unpaidbreakFlag) {
-		this.unpaidbreakFlag = unpaidbreakFlag;
-	}
-
-	public float getTaxRate() {
-		return taxRate;
-	}
-
-	public void setTaxRate(float taxRate) {
-		this.taxRate = taxRate;
-	}
-
-	public float getBasicWage() {
-		return basicWage;
-	}
-
-	public void setBasicWage(float basicWage) {
-		this.basicWage = basicWage;
-	}
-
-	public String getBgColor() {
-		return bgColor;
-	}
-
-	public void setBgColor(String bgColor) {
-		this.bgColor = bgColor;
-	}
-
-	public String getPhone1() {
-		return phone1;
-	}
-
-	public void setPhone1(String phone1) {
-		this.phone1 = phone1;
-	}
-
-	public String getPhone2() {
-		return phone2;
-	}
-
-	public void setPhone2(String phone2) {
-		this.phone2 = phone2;
-	}
-
-	public String getAddr1() {
-		return addr1;
-	}
-
-	public void setAddr1(String addr1) {
-		this.addr1 = addr1;
-	}
-
-	public String getAddr2() {
-		return addr2;
-	}
-
-	public void setAddr2(String addr2) {
-		this.addr2 = addr2;
-	}
-
-	public String getAddr3() {
-		return addr3;
-	}
-
-	public void setAddr3(String addr3) {
-		this.addr3 = addr3;
-	}
-
-	public String getStus() {
-		return stus;
-	}
-
-	public void setStus(String stus) {
-		this.stus = stus;
-	}
-
-	public Date getCreated() {
-		return created;
-	}
-
-	public void setCreated(Date created) {
-		this.created = created;
-	}
-
-	public Date getEdited() {
-		return edited;
-	}
-
-	public void setEdited(Date edited) {
-		this.edited = edited;
-	}
-
-	@Override
-	public String toString() {
-		return "\nActorEty [seq=" + seq + ", userId=" + userId + ", name=" + name + ", memo=" + memo + ", periodFrom="
-				+ periodFrom + ", periodTo=" + periodTo + ", workTimeUnit=" + workTimeUnit + ", alarmBefore="
-				+ alarmBefore + ", unpaidbreakFlag=" + unpaidbreakFlag + ", taxRate=" + taxRate + ", basicWage="
-				+ basicWage + ", bgColor=" + bgColor + ", phone1=" + phone1 + ", phone2=" + phone2 + ", addr1=" + addr1
-				+ ", addr2=" + addr2 + ", addr3=" + addr3 + ", stus=" + stus + ", created=" + created + ", edited="
-				+ edited + "]";
-	}
-
 }
