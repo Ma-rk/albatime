9f81d4190c43723809a9d62c9d46cf221bfd59ff
Mark K <k.mark.kr@gmail.com>
2015-07-27 20:45:59 +0900

    created ScheEty. close #64

---
+++ albatime_web/src/at/model/ScheEty.java
@@ -0,0 +1,44 @@
+package at.model;
+
+import java.sql.Time;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.joda.time.DateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScheEty {
+	long seq;
+
+	@NotNull
+	long actorId;
+
+	@Size(min = 1, max = 256)
+	String memo;
+
+	@NotNull
+	Time scheTimeFrom;
+
+	@NotNull
+	Time scheTimeTo;
+
+	@Min(0)
+	@Max(120)
+	String unpaidbreakMin;
+
+	@Pattern(regexp = "[A-Z]{3}[_][A-Z]{3}[_][0-9]{2}")
+	String stus;
+
+	DateTime created;
+	DateTime edited;
+}
