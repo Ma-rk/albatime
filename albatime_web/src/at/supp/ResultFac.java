c276d4dcff76dd2cdfb9a586778cf0844ef330cb
Mark K <k.mark.kr@gmail.com>
2015-08-03 21:54:09 +0900

    created class: ResultFactory. close #77

---
+++ albatime_web/src/at/supp/ResultFac.java
@@ -0,0 +1,27 @@
+package at.supp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResultFac {
+	public static Map<String, Object> rf(Object data) {
+		return rf(true, "NO_ERROR", data);
+	}
+
+	public static Map<String, Object> rf(Boolean result, String errorCoe) {
+		return rf(false, errorCoe, 0);
+	}
+
+	public static Map<String, Object> rf(Boolean result, String errorCoe, Object data) {
+		HashMap<String, Object> resultMap = new HashMap<>();
+		resultMap.put("result", result);
+		if (result) {
+			resultMap.put("errorCode", "NO_ERROR");
+			resultMap.put("data", data);
+		} else {
+			resultMap.put("errorCode", errorCoe);
+			resultMap.put("data", 0);
+		}
+		return resultMap;
+	}
+}
