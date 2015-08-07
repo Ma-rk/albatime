e005112c848ff83ad83007975d9c268e9cbed33e
Mark K <k.mark.kr@gmail.com>
2015-08-07 19:49:11 +0900

    improved VisitLogEty: applied Hibernate. close #81

--- albatime_web/src/at/model/VisitLogEty.java
+++ albatime_web/src/at/model/VisitLogEty.java
@@ -1,47 +1,43 @@
 package at.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 
+import lombok.Data;
+
+@Entity
+@Data
 public class VisitLogEty {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long visitSeq;
+
 	private String visitorIp;
-	private String requestedPage;
-	private String requestMethod;
+	private String reqPage;
+	private String reqMethod;
 	private String userAgent;
-	private DateTime requestedTime;
 
-	public VisitLogEty(String visitorIp, String requestedPage, String requestMethod, String userAgent) {
+	@Column(insertable = false, updatable = false)
+	private DateTime visitDatetime;
+
+	public VisitLogEty(String visitorIp, String reqPage, String reqMethod, String userAgent) {
 		this.visitorIp = visitorIp;
-		this.requestedPage = requestedPage;
-		this.requestMethod = requestMethod;
+		this.reqPage = reqPage;
+		this.reqMethod = reqMethod;
 		this.userAgent = userAgent;
-		this.requestedTime = new DateTime();
+		this.visitDatetime = new DateTime();
 	}
 
-	public String getVisitorIp() {
-		return visitorIp;
-	}
-
-	public String getRequestedPage() {
-		return requestedPage;
-	}
-
-	public String getRequestMethod() {
-		return requestMethod;
-	}
-
-	public String getUserAgent() {
-		return userAgent;
-	}
-
-	public DateTime getRequestedTime() {
-		return requestedTime;
-	}
-
-	@Override
 	public String toString() {
-		return "\nVisitLogEty [visitorIp=" + visitorIp + ", requestedPage=" + requestedPage + ", requestMethod="
-				+ requestMethod + ", userAgent=" + userAgent + ", requestedTime="
-				+ requestedTime.toString(DateTimeFormat.forPattern("yyyy-MM-dd Z kk:mm:ss:SS")) + "]";
+		return "\nVisitLogEty [visitorIp=" + visitorIp + ", requestedPage=" + reqPage + ", requestMethod="
+				+ reqMethod + ", userAgent=" + userAgent + ", requestedTime="
+				+ visitDatetime.toString(DateTimeFormat.forPattern("yyyy-MM-dd Z kk:mm:ss:SS")) + "]";
 	}
 }
