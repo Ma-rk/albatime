2a13a7bac0f83c03ad7109049e4d8dbdb8d42f90
Mark K <k.mark.kr@gmail.com>
2015-08-10 13:41:05 +0900

    improved CardEty: separated field for hour, min. added annotation for Hibernate. close #87

--- albatime_web/src/at/model/CardEty.java
+++ albatime_web/src/at/model/CardEty.java
@@ -1,11 +1,15 @@
 package at.model;
 
-import java.sql.Time;
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import at.com.CC;
 import lombok.AllArgsConstructor;
@@ -15,22 +19,36 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class CardEty {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long seq;
 
 	@NotNull
 	private long actorSeq;
 
 	@NotNull
+	@Size(min = 1, max = 32)
 	private String name;
 
+	@Size(min = 0, max = 256)
 	private String memo;
 
-	@NotNull
-	private Time timeFrom;
+	@Min(0)
+	@Max(23)
+	private int hourFrom;
+	@Min(0)
+	@Max(59)
+	private int minFrom;
 
+	@Min(0)
+	@Max(23)
+	private int hourTo;
+	@Min(0)
+	@Max(59)
 	@NotNull
-	private Time timeTo;
+	private int minTo;
 
 	@Min(0)
 	@Max(120)
@@ -39,7 +57,9 @@ public class CardEty {
 	@Pattern(regexp = "[A-Z]{3}[_][A-Z]{3}[_][0-9]{2}")
 	private String stus;
 
+	@Column(insertable = false, updatable = false)
 	private String created;
+	@Column(insertable = false, updatable = false)
 	private String edited;
 
 	public void setAsNormalStus() {
